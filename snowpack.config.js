e073df804c93f86f60827c0cde1fbd33793f1bdf
Riaz Laskar <riazcool77@gmail.com>
2021-03-27 13:26:18 +0530

    Initial Commit

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,7 +1,8 @@
 /** @type {import("snowpack").SnowpackUserConfig } */
 module.exports = {
   mount: {
-    /* ... */
+    src: '/dist',
+    public: '/',
   },
   plugins: [
     /* ... */
---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Banner from './banner';
+
+function App() {
+  return (
+    <div>
+      <Banner message="Banner component" />
+    </div>
+  );
+}
+
+export default App;
